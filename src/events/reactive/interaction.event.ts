948e9643d6f635559118c2962ef0c3745f4380c6
Manuel <Manusolerj@gmail.com>
2025-05-19 17:16:10 +0200

    Chore: - Added slap and cheer to interaction listener.

--- src/events/reactive/interaction.event.ts
+++ src/events/reactive/interaction.event.ts
@@ -5,6 +5,8 @@ import { execute as beg } from "@commands/basic/beg.command";
 import { execute as info } from "@commands/basic/chaos-info.command";
 import { execute as control } from "@commands/basic/chaos-control.command";
 import { execute as inspection } from "@commands/basic/chaos-inspection.command";
+import { execute as slap } from "@commands/basic/slap.command";
+import { execute as cheer } from "@commands/basic/cheer.command";
 import { execute as speak } from "@commands/basic/speak.command";
 import { execute as timer } from "@commands/basic/timer.command";
 
@@ -30,6 +32,12 @@ export default function interactionListener(client: Client) {
         break;
       case "speak":
         await speak(interaction);
+        break;
+      case "slap":
+        await slap(interaction);
+        break;
+      case "cheer":
+        await cheer(interaction);
         break;
       case "timer":
         await timer(interaction);
