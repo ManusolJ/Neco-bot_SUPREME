69e7047d2228f4fcab47ae15591d475b6a8ee63f
Manuel <Manusolerj@gmail.com>
2025-07-08 01:32:45 +0200

    refactor: improve altar event handling by extracting constants and enhancing readability

--- src/events/reactive/altar.event.ts
+++ src/events/reactive/altar.event.ts
@@ -1,4 +1,5 @@
-import { Client, Events } from "discord.js";
+import { Client, Events, Message, OmitPartialGroupDMChannel } from "discord.js";
+import { env } from "process";
 
 import MessageService from "@services/message.service";
 import NecoService from "@services/neco.service";
@@ -6,41 +7,76 @@ import chaosBuilder from "@utils/build-chaos.util";
 import randomMessageBuilder from "@utils/build-random-message.util";
 import reactionBuilder from "@utils/build-reaction.util";
 
+const NECO_ALTAR_CHANNEL_ID = env.NECO_ALTAR_CHANNEL;
+
+const MINIMUM_REWARD = 1;
+const MAXIMUM_REWARD = 5;
+
+const MESSAGE_CASE = "altar";
+
 export default function altarEvent(client: Client): void {
-  client.on(Events.MessageCreate, async (message) => {
-    if (message && message.channelId === process.env.NECO_ALTAR_CHANNEL) {
-      const necoService = await NecoService.getInstance();
-      const author = message.author;
-      const guild = message.guild;
+  client.on(Events.MessageCreate, async (message) => handleAltarEvent(message));
+}
 
-      if (author.bot || !guild) return;
+async function handleAltarEvent(message: OmitPartialGroupDMChannel<Message<boolean>>) {
+  if (message && message.channelId === NECO_ALTAR_CHANNEL_ID) {
+    const necoService = await NecoService.getInstance();
 
-      const hasMedia = message.embeds.length > 0 || message.attachments.size > 0;
-      const hasTwitterLink = /https?:\/\/(x|twitter|vxtwitter)\.com\/\S+/i.test(message.content);
-      const isPost = hasMedia || hasTwitterLink;
+    const author = message.author;
+    const guild = message.guild;
 
-      if (!isPost) return;
-
-      const channel = guild.channels.cache.get(message.channel.id);
-
-      if (!channel || !channel.isTextBased()) return;
-
-      const msgService = new MessageService(channel);
-
-      await necoService.manipulateAgentBalance(author.id, chaosBuilder(1, 5));
-
-      const msg = randomMessageBuilder("altar", author);
-
-      if (!msg) return;
-
-      try {
-        await msgService.send(msg);
-        await reactionBuilder(message);
-      } catch (e) {
-        console.error(e);
-        const errorMsg = "...Pero no he podido darte puntos! No preguntes porque, no lo se! Nyahahaahaha!";
-        await msgService.sendError(msg + errorMsg);
-      }
+    if (!guild || !author) {
+      console.error("Hubo un error al intentar recuperar el autor o el servidor del mensaje.");
+      return;
     }
-  });
+
+    if (author.bot) return;
+
+    const hasMedia = message.embeds.length > 0 || message.attachments.size > 0;
+
+    const hasTwitterLink = /https?:\/\/(x|twitter|vxtwitter)\.com\/\S+/i.test(message.content);
+
+    const isPost = hasMedia || hasTwitterLink;
+
+    if (!isPost) return;
+
+    const channel = guild.channels.cache.get(message.channel.id);
+
+    if (!channel || !channel.isTextBased()) {
+      console.error("El canal del mensaje no es un canal de texto válido.");
+      return;
+    }
+
+    const msgService = new MessageService(channel);
+
+    const agentExists = await necoService.checkAgentExists(author.id);
+
+    let agent;
+
+    if (!agentExists) {
+      await necoService.createAgent(author.id);
+    }
+
+    agent = await necoService.getAgent(author.id);
+
+    if (!agent) {
+      console.error("No se pudo recuperar el agente del autor de la db.");
+      return;
+    }
+
+    const newBalance = agent.balance + chaosBuilder(MINIMUM_REWARD, MAXIMUM_REWARD);
+
+    await necoService.manipulateAgentBalance(author.id, newBalance);
+
+    const msg = randomMessageBuilder(MESSAGE_CASE, author);
+
+    if (!msg) {
+      console.error("No se pudo construir un mensaje aleatorio para el altar.");
+      return;
+    }
+
+    await msgService.send(msg);
+
+    await reactionBuilder(message);
+  }
 }
