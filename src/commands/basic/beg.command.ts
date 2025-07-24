828c10e4d965887316c4fd07d26b997581d529ca
Manuel <Manusolerj@gmail.com>
2025-07-24 20:48:58 +0200

    refactir: beg command  improved error handling and streamline agent management

--- src/commands/basic/beg.command.ts
+++ src/commands/basic/beg.command.ts
@@ -6,10 +6,13 @@ import NecoService from "@services/neco.service";
 import chaosBuilder from "@utils/build-chaos.util";
 import randomMessageBuilder from "@utils/build-random-message.util";
 import { isUserLocked, lockUser, unlockUser } from "@utils/lock-user.util";
+import Agent from "@interfaces/agent.interface";
 
 export const data = new SlashCommandBuilder()
   .setName("beg")
   .setDescription("Pidele a Neco-arc unas cuantas monedas como el vagabundo que eres.");
+
+const CULTIST_ROLE = process.env.CULTIST_ROLE;
 
 const IMAGE_PATH = "public/img/";
 const IMAGE_FEEDBACK = path.join(IMAGE_PATH, "pilk.jpg");
@@ -24,16 +27,19 @@ export async function execute(interaction: ChatInputCommandInteraction) {
   const interactionService = new InteractionService(interaction);
 
   const author = interaction.user;
-  let isLocked = false;
 
   try {
     if (!interaction.inGuild() || !interaction.guild) {
       throw new Error("Interaction used outside server.");
     }
 
-    const member = await interaction.guild.members.fetch(author.id);
+    if (!CULTIST_ROLE) {
+      throw new Error("role environment variable missing in beg command.");
+    }
 
-    if (!member) {
+    const guildMember = await interaction.guild.members.fetch(author.id);
+
+    if (!author) {
       const errorMsg = "NYAAAHA! Hubo un problema intentado recuperar tu informacion!";
       return await interactionService.errorReply(errorMsg);
     }
@@ -44,44 +50,44 @@ export async function execute(interaction: ChatInputCommandInteraction) {
     }
 
     lockUser(author.id);
-    isLocked = true;
 
-    let agent = await necoService.getAgent(member.id);
+    let agent: Agent | null = await necoService.getAgent(author.id);
 
     if (!agent) {
-      await necoService.createAgent(author.id);
-      agent = await necoService.getAgent(author.id);
+      agent = await necoService.createAgent(author.id);
     }
 
     if (!agent) throw new Error("Agent creation failed");
 
     if (agent.begged) {
-      const feedbackMsg = "¿Otra vez pidiendo? Nyah~ ¡Eso no es muy digno del caos! Espera hasta el siguiente dia!";
-      const image = path.resolve(IMAGE_FEEDBACK);
+      const feedbackMsg: string = `¿Otra vez pidiendo? Nyah~ ¡Eso no es muy digno del caos! Espera hasta el siguiente dia!`;
+      const shame: number = agent.shame + 1;
+      await necoService.manipulateAgentShame(author.id, shame);
+      const image: string = path.resolve(IMAGE_FEEDBACK);
       return await interactionService.feedbackReply(feedbackMsg, [image]);
     }
 
     if (agent.balance >= LIMIT) {
-      const feedbackMsg = "¿¡HUH!? Tu ya tienes suficientes monedas! A pedir a la iglesia.";
-      const image = path.resolve(IMAGE_FEEDBACK);
+      const feedbackMsg: string = `¿¡HUH!? Tu ya tienes suficientes monedas! A pedir a la iglesia.`;
+      const image: string = path.resolve(IMAGE_FEEDBACK);
       return await interactionService.feedbackReply(feedbackMsg, [image]);
     }
 
-    let success = Math.random() < 0.8;
+    let success: boolean = Math.random() < 0.8;
 
-    let secondTry = false;
+    let secondTry: boolean = false;
 
-    const cultistRole = process.env.CULTIST_ROLE;
+    const cultistRole: string = CULTIST_ROLE;
 
-    if (!success && cultistRole && member.roles.cache.has(cultistRole)) {
-      const replyMsg = "Fallaste! Menuda skill iss- Ah, espera... Que eres uno de mis fieles. Venga, otro intento...";
+    if (!success && cultistRole && guildMember.roles.cache.has(cultistRole)) {
+      const replyMsg: string = `Fallaste! Menuda skill iss- Ah, espera... Que eres uno de mis fieles. Venga, otro intento...`;
       await interactionService.standardReply(replyMsg);
       success = Math.random() < 0.6;
       secondTry = true;
     }
 
     if (!success) {
-      await necoService.manipulateAgentBegState(member.id, true);
+      await necoService.manipulateAgentBegState(author.id, true);
       const replyMsg = randomMessageBuilder("begFail");
       const image = path.resolve(IMAGE_FAIL);
       return secondTry
@@ -89,9 +95,10 @@ export async function execute(interaction: ChatInputCommandInteraction) {
         : interactionService.filesReply(replyMsg, [image]);
     }
 
-    const awarded = chaosBuilder(MINIMUM_AWARDED, MAXIMUM_AWARDED);
-    await necoService.manipulateAgentBegState(member.id, true);
-    await necoService.manipulateAgentBalance(member.id, agent.balance + awarded);
+    const awarded: number = chaosBuilder(MINIMUM_AWARDED, MAXIMUM_AWARDED);
+    const newBalance: number = agent.balance + awarded;
+    await necoService.manipulateAgentBegState(author.id, true);
+    await necoService.manipulateAgentBalance(author.id, newBalance);
     const replyMsg = `${randomMessageBuilder(data.name, author)} ${
       awarded > 1 ? `${awarded} puntos.` : `1 punto lmao.`
     }`;
@@ -100,11 +107,10 @@ export async function execute(interaction: ChatInputCommandInteraction) {
       ? await interactionService.followReply(replyMsg)
       : await interactionService.standardReply(replyMsg);
   } catch (e) {
-    const errorMsg = `Hubo un PROBLEMA! El siguiente: `;
+    const errorMsg: string = `Error executing beg command: `;
     console.error(errorMsg, e);
-    await interactionService.errorReply("¡NYAAA! Algo salio MUY mal. Intenta de nuevo mas tarde... O diselo a Manuel.");
   } finally {
-    if (isLocked) {
+    if (isUserLocked(interaction.user.id)) {
       unlockUser(interaction.user.id);
     }
   }
