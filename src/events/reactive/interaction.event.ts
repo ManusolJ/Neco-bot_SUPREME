5eb564058d64bcda8099111576b632497e9d45d6
Manuel <Manusolerj@gmail.com>
2025-07-30 23:37:22 +0200

    fix: Remove unused chaos-control and chaos-inspection commands from interaction handler

--- src/events/reactive/interaction.event.ts
+++ src/events/reactive/interaction.event.ts
@@ -4,8 +4,6 @@ import { Client, Events } from "discord.js";
 import { execute as ask } from "@commands/basic/ask.command";
 import { execute as beg } from "@commands/basic/beg.command";
 import { execute as info } from "@commands/basic/chaos-info.command";
-import { execute as control } from "@commands/basic/chaos-control.command";
-import { execute as inspection } from "@commands/basic/chaos-inspection.command";
 import { execute as slap } from "@commands/basic/slap.command";
 import { execute as cheer } from "@commands/basic/cheer.command";
 import { execute as speak } from "@commands/basic/speak.command";
@@ -13,6 +11,7 @@ import { execute as timer } from "@commands/basic/timer.command";
 import { execute as zaza } from "@commands/basic/zaza.command";
 import { execute as monster } from "@commands/advanced/monster.command";
 import { execute as trade } from "@commands/advanced/trade.command";
+import { execute as debug } from "@commands/advanced/debug.command";
 
 /**
  * Central interaction router for slash commands
@@ -36,12 +35,6 @@ export default function interactionListener(client: Client) {
       case "chaos-info":
         await info(interaction);
         break;
-      case "chaos-control":
-        await control(interaction);
-        break;
-      case "chaos-inspection":
-        await inspection(interaction);
-        break;
       case "speak":
         await speak(interaction);
         break;
@@ -62,6 +55,9 @@ export default function interactionListener(client: Client) {
         break;
       case "trade":
         await trade(interaction);
+        break;
+      case "debug":
+        await debug(interaction);
         break;
     }
   });
