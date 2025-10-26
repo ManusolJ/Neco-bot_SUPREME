3abd9a6c9967b33c309d134ffa7c06a516a9c4a7
Manu <mansoljua@alu.edu.gva.es>
2025-10-26 07:10:38 +0100

    Change weekly lottery schedule to Fridays at 23:00

--- src/events/scheduled/weekly-lottery.event.ts
+++ src/events/scheduled/weekly-lottery.event.ts
@@ -59,7 +59,7 @@ const LOSER_POST: Record<"x2" | "x3" | "x5", string> = {
 export default function weeklyLottery(client: Client): void {
   client.once("ready", () => {
     // Every Friday at 23:00 PM Europe/Madrid
-    cron.schedule("0 23 * * SAT", async () => scheduledTask(client), {
+    cron.schedule("0 23 * * FRI", async () => scheduledTask(client), {
       timezone: "Europe/Madrid",
     });
   });
